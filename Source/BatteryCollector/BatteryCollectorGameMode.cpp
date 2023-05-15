b9ed9861b3215097c90b8dbe2236b50d6cd2b9f9
luozhi <929887908@qq.com>
2023-05-15 16:49:26 +0800

    在BatteryCollectorGameMode添加人物能量衰减,记得在构造函数设置PrimaryActorTick.bCanEverTick = true;

--- Source/BatteryCollector/BatteryCollectorGameMode.cpp
+++ Source/BatteryCollector/BatteryCollectorGameMode.cpp
@@ -4,12 +4,55 @@
 #include "BatteryCollectorCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
+#include "Kismet/GameplayStatics.h"
+
 ABatteryCollectorGameMode::ABatteryCollectorGameMode()
 {
+
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	//base decay rate
+	DecayRate = 0.01f;
+
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+//void ABatteryCollectorGameMode::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//	//Check that we are using the battery collector character
+//	ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+//	if (MyCharacter)
+//	{
+//		//if the character's power is positive
+//		if (MyCharacter->GetCurrentPower() > 0)
+//		{
+//			//decrease the character's power using the decay rate
+//			MyCharacter->UpdatePower(-DeltaTime * DecayRate * (MyCharacter->GetInitialPower()));
+//		}
+//	}
+//}
+
+
+void ABatteryCollectorGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Check that we are using the battery collector character
+	ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (MyCharacter)
+	{
+		// If our power is greater than needed to win, set the game's state to Won
+		if (MyCharacter)
+		{
+			// decrease the character's power using the decay rate
+			MyCharacter->UpdatePower(-DeltaTime * DecayRate * (MyCharacter->GetInitialPower()));
+		}
 	}
 }
